3ffca919b231736ba0b58f55095de4664958ad63
paneiOS <pane4846@gmail.com>
2025-01-07 00:31:01 +0900

    [MS-10][FEAT]RIBs 템플릿을 Tuist 템플릿으로 추가

--- Tuist/Templates/feature/feature.swift
+++ Tuist/Templates/feature/feature.swift
@@ -1,25 +1,36 @@
 //
 //  feature.swift
-//  Templates
+//  MySeconds
 //
 //  Created by 이정환 on 1/3/25.
 //
 
 import ProjectDescription
+import Foundation
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "mm/dd/yyyy"
+let currentDate = dateFormatter.string(from: Date())
+
+let userName = NSUserName()
 
 let nameAttribute: Template.Attribute = .required("name")
 
 let template = Template(
     description: "Creates a new feature module",
-    attributes: [nameAttribute],
+    attributes: [
+        nameAttribute,
+        .optional("userName", default: .string(userName)),
+        .optional("date", default: .string(currentDate))
+    ],
     items: [
         .file(
             path: "Modules/\(nameAttribute)/Project.swift",
-            templatePath: "Project.stencil"
+            templatePath: "stencils/Project.stencil"
         ),
         .file(
             path: "Modules/\(nameAttribute)/Sources/\(nameAttribute).swift",
-            templatePath: "Feature.stencil"
+            templatePath: "stencils/Feature.stencil"
         )
     ]
 )
---
+++ Tuist/Templates/ribs/ribs.swift
@@ -0,0 +1,53 @@
+//
+//  ribs.swift
+//  MySeconds
+//
+//  Created by 이정환 on 1/6/25.
+//
+
+import ProjectDescription
+import Foundation
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "mm/dd/yyyy"
+let currentDate = dateFormatter.string(from: Date())
+
+let userName = NSUserName()
+
+let nameAttribute: Template.Attribute = .required("name")
+
+let template = Template(
+    description: "ModernRIBs ownsView 기반 RIB 생성",
+    attributes: [
+        nameAttribute,
+        .optional("userName", default: .string(userName)),
+        .optional("date", default: .string(currentDate))
+    ],
+    items: [
+        .file(
+            path: "Modules/\(nameAttribute)/Project.swift",
+            templatePath: "stencils/Project.stencil"
+        ),
+        .file(
+            path: "Modules/\(nameAttribute)/Sources/\(nameAttribute)Builder.swift",
+            templatePath: "stencils/Builder.stencil"
+        ),
+        .file(
+            path: "Modules/\(nameAttribute)/Sources/\(nameAttribute)Router.swift",
+            templatePath: "stencils/Router.stencil"
+        ),
+        .file(
+            path: "Modules/\(nameAttribute)/Sources/\(nameAttribute)Interactor.swift",
+            templatePath: "stencils/Interactor.stencil"
+        ),
+        .file(
+            path: "Modules/\(nameAttribute)/Sources/\(nameAttribute)ViewController.swift",
+            templatePath: "stencils/ViewController.stencil"
+        ),
+        .file(
+            path: "Modules/\(nameAttribute)/Tests/\(nameAttribute)Tests.swift",
+            templatePath: "stencils/Tests.stencil"
+        )
+    ]
+)
+
