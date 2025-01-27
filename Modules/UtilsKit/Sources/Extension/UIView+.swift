ab2ff0a640e46545170a083f775e45ae039b45e8
이정환 <pane@hanatour.com>
2025-01-27 18:02:37 +0900

    [MS-16-VideoView][FEAT]그림자 추가 함수 구현

--- Modules/UtilsKit/Sources/Extension/UIView+.swift
+++ Modules/UtilsKit/Sources/Extension/UIView+.swift
@@ -7,8 +7,28 @@
 
 import UIKit
 
-extension UIView {
+public extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { self.addSubview($0) }
+    }
+
+    func applyShadow(
+        color: UIColor = .black,
+        x: CGFloat = 0,
+        y: CGFloat = 0,
+        blur: CGFloat = 0,
+        spread: CGFloat = 0
+    ) {
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = 1.0
+        layer.shadowOffset = CGSize(width: x, height: y)
+        layer.shadowRadius = blur / 2.0
+
+        if spread == 0 {
+            layer.shadowPath = nil
+        } else {
+            let rect = bounds.insetBy(dx: -spread, dy: -spread)
+            layer.shadowPath = UIBezierPath(rect: rect).cgPath
+        }
     }
 }
