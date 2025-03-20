53106ed52b856e00fb02265923c4a265d7775c7d
Ruel <c0407h@gmail.com>
2025-03-20 23:28:30 +0900

    [MS-19-Navigationbar][FEAT] MainNavigationBar 추가

---
+++ MySeconds/Modules/MySecondsKit/Sources/UINavigationBar/MainNavigationBar.swift
@@ -0,0 +1,89 @@
+//
+//  MainNavigationBar.swift
+//  MySecondsKit
+//
+//  Created by Chung Wussup on 3/18/25.
+//
+
+import UIKit
+
+public protocol MainNavigationBarDelegate: AnyObject {
+    func didTapBackButton()
+}
+
+public final class MainNavigationBar: UINavigationBar {
+    
+    // MARK: - Properties
+    public weak var navigationDelegate: MainNavigationBarDelegate?
+    
+    private lazy var backButton: UIButton = {
+        let action = UIAction(image: UIImage(resource: ImageResource.chevronLeft)
+            .withRenderingMode(.alwaysTemplate)) { _ in
+                self.navigationDelegate?.didTapBackButton()
+            }
+        let button = UIButton(type: .custom, primaryAction: action)
+        button.tintColor = .neutral800
+        return button
+    }()
+    
+    // MARK: - Initializers
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setupNavigationBar()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupNavigationBar()
+    }
+    
+    // MARK: - Setup Methods
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.black,
+            .font: UIFont.systemFont(ofSize: 16, weight: .regular)
+        ]
+        self.standardAppearance = appearance
+        self.scrollEdgeAppearance = appearance
+        self.compactAppearance = appearance
+    }
+    
+    // MARK: - Public Methods
+    public func configure(
+        title: String,
+        rightButtons: [(UIImage, (() -> Void)?)]? = nil
+    ) {
+        let naviItem = UINavigationItem(title: title)
+        
+        naviItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+        naviItem.rightBarButtonItems = setupRightButtonItems(buttons: rightButtons)
+        
+        self.items = [naviItem]
+    }
+    
+    // MARK: - Private Methods
+    private func setupRightButtonItems(
+        buttons: [(UIImage, (() -> Void)?)]?
+    ) -> [UIBarButtonItem]? {
+        guard let buttons, !buttons.isEmpty else { return nil }
+        
+        return buttons.map { image, action in
+            let button = createButton(image: image, action: action)
+            return UIBarButtonItem(customView: button)
+        }
+    }
+    
+    private func createButton(image: UIImage, action: (() -> Void)?) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.setImage(image.withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = .black
+        
+        if let action {
+            button.addAction(UIAction { _ in action() }, for: .touchUpInside)
+        }
+        return button
+    }
+}
