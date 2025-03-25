a94db5781901bc23ad36b48eb48d37e2fb9e5d11
Ruel <c0407h@gmail.com>
2025-03-25 23:07:50 +0900

    [MS-19-Navigationbar][FEAT] MainNavigationController 추가

---
+++ MySeconds/Modules/MySecondsKit/Sources/UINavigationBar/MSNavigationController.swift
@@ -0,0 +1,45 @@
+//
+//  MSNavigationController.swift
+//  MySecondsKit
+//
+//  Created by Chung Wussup on 3/25/25.
+//
+
+import UIKit
+
+import SnapKit
+
+public final class MSNavigationController: UINavigationController {
+    
+    private let msNaivagationBar = MainNavigationBar()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setNavigationBarHidden(true, animated: false)
+        self.setupMSNavigationBar()
+    }
+    
+    private func setupMSNavigationBar() {
+        view.addSubview(msNaivagationBar)
+        msNaivagationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(44)
+        }
+        
+        msNaivagationBar.backButton.target = self
+        msNaivagationBar.backButton.action = #selector(didTapBackButton)
+    }
+    
+    public func pushViewController(_ viewController: UIViewController,
+                                   title: String,
+                                   rightButtons: [(UIImage, (() -> Void)?)]? = nil,
+                                   animated: Bool) {
+        super.pushViewController(viewController, animated: animated)
+        msNaivagationBar.configure(title: title, rightButtons: rightButtons)
+    }
+    
+    @objc private func didTapBackButton() {
+        self.popViewController(animated: true)
+    }
+}
