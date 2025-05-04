7db985dd73843d2ef9469488379650731c5d0059
Ruel <c0407h@gmail.com>
2025-05-04 21:07:10 +0900

    [MS-19-Navigationbar][CHOR] MSNavigationController 제거

--- MySeconds/Modules/MySecondsKit/Sources/UINavigationBar/MSNavigationController.swift
+++ MySeconds/Modules/MySecondsKit/Sources/UINavigationBar/MSNavigationController.swift
@@ -5,42 +5,46 @@
 //  Created by Chung Wussup on 3/25/25.
 //
 
-import UIKit
-
-import SnapKit
-
-public final class MSNavigationController: UINavigationController {
-
-    private let msNavigationBar = MainNavigationBar()
-
-    override public func viewDidLoad() {
-        super.viewDidLoad()
-        self.setNavigationBarHidden(true, animated: false)
-        self.setupMSNavigationBar()
-    }
-
-    private func setupMSNavigationBar() {
-        view.addSubview(self.msNavigationBar)
-        self.msNavigationBar.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(44)
-        }
-        self.msNavigationBar.backButton.target = self
-        self.msNavigationBar.backButton.action = #selector(self.didTapBackButton)
-    }
-    
-    public func msPushViewController(
-        _ viewController: UIViewController,
-        title: String,
-        rightButtons: [(UIImage, (() -> Void)?)]? = nil,
-        animated: Bool
-    ) {
-        super.pushViewController(viewController, animated: animated)
-        self.msNavigationBar.configure(title: title, rightButtons: rightButtons)
-    }
-
-    @objc private func didTapBackButton() {
-        self.popViewController(animated: true)
-    }
-}
+// import Combine
+// import UIKit
+//
+// import SnapKit
+//
+// public final class MSNavigationController: UINavigationController {
+//
+//    private var cancellables = Set<AnyCancellable>()
+//    private let msNavigationBar = MainNavigationBar()
+//
+//    override public func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.setNavigationBarHidden(true, animated: false)
+//        self.setupMSNavigationBar()
+//    }
+//
+//    private func setupMSNavigationBar() {
+//        view.addSubview(self.msNavigationBar)
+//        self.msNavigationBar.snp.makeConstraints {
+//            $0.top.equalTo(view.safeAreaLayoutGuide)
+//            $0.leading.trailing.equalToSuperview()
+//            $0.height.equalTo(44)
+//        }
+//
+//        self.msNavigationBar.backButtonTapped
+//            .sink { [weak self] in
+//                self?.popViewController(animated: true)
+//            }
+//            .store(in: &cancellables)
+//
+//    }
+//
+//    public func msPushViewController(
+//        _ viewController: UIViewController,
+//        title: String,
+//        rightButtons: [(UIImage, (() -> Void)?)]? = nil,
+//        animated: Bool
+//    ) {
+//        super.pushViewController(viewController, animated: animated)
+//        self.msNavigationBar.configure(title: title, rightButtons: rightButtons)
+//    }
+//
+// }
