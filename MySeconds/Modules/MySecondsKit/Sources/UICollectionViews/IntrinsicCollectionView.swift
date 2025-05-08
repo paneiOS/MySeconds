08e4ffab7e69b312a75d28e201b59927c40aebd9
paneiOS <pane4846@gmail.com>
2025-05-08 23:48:14 +0900

    [MS-25-CustomSegment][FEAT]IntrinsicCollectionView 구현, 스택뷰에서 사용시 intrinsicContentSize를 고정하기 위한 커스텀 컬렉션뷰

---
+++ MySeconds/Modules/MySecondsKit/Sources/UICollectionViews/IntrinsicCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  IntrinsicCollectionView.swift
+//  MySecondsKit
+//
+//  Created by 이정환 on 5/8/25.
+//
+
+import UIKit
+
+public final class IntrinsicCollectionView: UICollectionView {
+    override public var intrinsicContentSize: CGSize {
+        contentSize
+    }
+
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        invalidateIntrinsicContentSize()
+    }
+}
