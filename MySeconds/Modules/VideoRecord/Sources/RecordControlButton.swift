d1b4956383476c03d39840c7a36c573aa2b9b70e
Ruel <c0407h@gmail.com>
2025-05-25 22:57:57 +0900

    [MS-31-RecordControl][FEAT] 카메라 컨트롤 UI 구현

---
+++ MySeconds/Modules/VideoRecord/Sources/RecordControlButton.swift
@@ -0,0 +1,66 @@
+//
+//  RecordControlButton.swift
+//  VideoRecord
+//
+//  Created by Chung Wussup on 5/25/25.
+//
+
+import UIKit
+
+import ResourceKit
+
+final class RecordControlButton: UIButton {
+    enum ButtonType {
+        case record, ratio, timer, flip, album
+    }
+
+    init(
+        type: ButtonType,
+        size: CGFloat,
+        cornerRadius: CGFloat,
+        borderColor: UIColor? = nil,
+        borderWidth: CGFloat = 0
+    ) {
+        super.init(frame: .zero)
+
+        layer.cornerRadius = cornerRadius
+        if let borderColor {
+            layer.borderColor = borderColor.cgColor
+            layer.borderWidth = borderWidth
+        }
+
+        snp.makeConstraints {
+            $0.size.equalTo(size)
+        }
+
+        switch type {
+        case .record:
+            backgroundColor = .red600
+
+        case .ratio:
+            backgroundColor = .neutral100
+            setTitle("1:1", for: .normal)
+            setTitleColor(.neutral950, for: .normal)
+
+        case .timer:
+            backgroundColor = .neutral100
+            titleLabel?.numberOfLines = 2
+            titleLabel?.textAlignment = .center
+
+        case .flip:
+            backgroundColor = .neutral100
+            setImage(ResourceKitAsset.refreshCcw.image, for: .normal)
+            tintColor = .neutral950
+
+        case .album:
+            backgroundColor = .neutral100
+            imageView?.contentMode = .scaleAspectFill
+            clipsToBounds = true
+        }
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- MySeconds/Modules/VideoRecord/Sources/VideoRecordViewController.swift
+++ MySeconds/Modules/VideoRecord/Sources/VideoRecordViewController.swift
@@ -5,9 +5,11 @@
 //  Created by chungwussup on 05/19/2025.
 //
 
+import Combine
 import UIKit
 
 import BaseRIBsKit
+import SnapKit
 
 import MySecondsKit
 import ResourceKit
@@ -17,6 +19,63 @@ protocol VideoRecordPresentableListener: AnyObject {}
 final class VideoRecordViewController: BaseViewController, VideoRecordPresentable, VideoRecordViewControllable, NavigationConfigurable {
 
     weak var listener: VideoRecordPresentableListener?
+
+    private let recordControlView = RecordControlView()
+
+    override func setupUI() {
+        self.view.backgroundColor = .white
+        self.view.addSubviews(self.recordControlView)
+
+        self.recordControlView.snp.makeConstraints {
+            $0.height.equalTo(136)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+
+    override func bind() {
+        self.recordControlView.recordTapPublisher
+            .sink(receiveValue: { [weak self] _ in
+                guard let self else { return }
+                // TODO: 녹화 기능
+                print("녹화")
+                self.recordControlView.recordDuration = 3
+            })
+            .store(in: &cancellables)
+
+        self.recordControlView.flipTapPublisher
+            .sink(receiveValue: { [weak self] _ in
+                guard let self else { return }
+                // TODO: 카메라 플립 기능
+                print("카메라 촬영 화면 변경")
+            })
+            .store(in: &cancellables)
+
+        self.recordControlView.ratioTapPublisher
+            .sink(receiveValue: { [weak self] _ in
+                guard let self else { return }
+                // TODO: 카메라 프리뷰화면 비율 변경 기능
+                self.recordControlView.setRatioButtonText()
+                print("카메라 비율 변경")
+            })
+            .store(in: &cancellables)
+
+        self.recordControlView.timerTapPublisher
+            .sink(receiveValue: { [weak self] _ in
+                guard let self else { return }
+                // TODO: 타이머 변경 기능
+                self.recordControlView.setTimerButtonText(seconds: "4초")
+                print("타이머 변경")
+            })
+            .store(in: &cancellables)
+
+        self.recordControlView.albumTapPublisher
+            .sink(receiveValue: { [weak self] _ in
+                guard let self else { return }
+                print("Tap Album Button")
+            })
+            .store(in: &cancellables)
+    }
 
     func navigationConfig() -> NavigationConfig {
         NavigationConfig(
