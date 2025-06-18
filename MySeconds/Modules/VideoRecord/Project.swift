285b3f9dc4de978fdf44c624a8c782673543b61c
Ruel <c0407h@gmail.com>
2025-06-18 15:18:10 +0900

    [MS-35-VideoRecording][CHOR] VideoDraftStorage 모듈 프로젝트 추가

--- MySeconds/Modules/VideoRecord/Project.swift
+++ MySeconds/Modules/VideoRecord/Project.swift
@@ -23,6 +23,7 @@ let project = Project(
                 .project(target: "BaseRIBsKit", path: "../BaseRIBsKit"),
                 .project(target: "MySecondsKit", path: "../MySecondsKit"),
                 .project(target: "ResourceKit", path: "../ResourceKit"),
+                .project(target: "VideoDraftStorage", path: "../../Core/VideoDraftStorage"),
                 .project(target: "UtilsKit", path: "../UtilsKit")
             ]
         ),
