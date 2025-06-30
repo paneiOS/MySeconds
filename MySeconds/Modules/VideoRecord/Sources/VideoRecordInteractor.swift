74be27ae0bc44fdd46028c26087c602f19e549d0
Ruel <c0407h@gmail.com>
2025-06-30 14:22:12 +0900

    [MS-35-VideoRecording][REFACTOR] index 안전한 접근을 위해 safe 사용

--- MySeconds/Modules/VideoRecord/Sources/VideoRecordInteractor.swift
+++ MySeconds/Modules/VideoRecord/Sources/VideoRecordInteractor.swift
@@ -24,58 +24,58 @@ protocol VideoRecordPresentable: Presentable {
 public protocol VideoRecordListener: AnyObject {}
 
 final class VideoRecordInteractor: PresentableInteractor<VideoRecordPresentable>, VideoRecordInteractable, VideoRecordPresentableListener {
-
+    
     private let videoDraftStorage = try? VideoDraftStorage()
-
+    
     private let component: VideoRecordComponent
     private let recordingManager: VideoRecordingManagerProtocol
-
+    
     private let timerButtonTextSubject = PassthroughSubject<Int, Never>()
     public var timerButtonTextPublisher: AnyPublisher<Int, Never> {
         self.timerButtonTextSubject.eraseToAnyPublisher()
     }
-
+    
     private let ratioButtonTextSubject = PassthroughSubject<String, Never>()
     public var ratioButtonTextPublisher: AnyPublisher<String, Never> {
         self.ratioButtonTextSubject.eraseToAnyPublisher()
     }
-
+    
     private let aspectRatioSubject = CurrentValueSubject<AspectRatio, Never>(.oneToOne)
     var aspectRatioPublisher: AnyPublisher<AspectRatio, Never> {
         self.aspectRatioSubject.eraseToAnyPublisher()
     }
-
+    
     private let isRecordingSubject = CurrentValueSubject<Bool, Never>(false)
     public var isRecordingPublisher: AnyPublisher<Bool, Never> {
         self.isRecordingSubject.eraseToAnyPublisher()
     }
-
+    
     private let recordDurationSubject = PassthroughSubject<TimeInterval, Never>()
     public var recordDurationPublisher: AnyPublisher<TimeInterval, Never> {
         self.recordDurationSubject.eraseToAnyPublisher()
     }
-
+    
     private let videosSubject = PassthroughSubject<[VideoDraft], Never>()
     public var videosPublisher: AnyPublisher<[VideoDraft], Never> {
         self.videosSubject.eraseToAnyPublisher()
     }
-
+    
     private let cameraAuthorizationSubject = PassthroughSubject<Bool, Never>()
     var cameraAuthorizationPublisher: AnyPublisher<Bool, Never> {
         self.cameraAuthorizationSubject.eraseToAnyPublisher()
     }
-
+    
     private var currentDurationIndex = 0
     private let durationOptions: [Int] = [1, 2, 3]
     private var currentAspectRatio: AspectRatio = .oneToOne
-
+    
     private let videoSubject = CurrentValueSubject<[VideoDraft], Never>([])
-
+    
     private var cancellables = Set<AnyCancellable>()
-
+    
     weak var router: VideoRecordRouting?
     weak var listener: VideoRecordListener?
-
+    
     init(
         presenter: VideoRecordPresentable,
         component: VideoRecordComponent,
@@ -85,10 +85,10 @@ final class VideoRecordInteractor: PresentableInteractor<VideoRecordPresentable>
         self.recordingManager = recordingManager
         super.init(presenter: presenter)
         presenter.listener = self
-
+        
         self.bind()
     }
-
+    
     private func bind() {
         self.videoSubject
             .sink(receiveValue: { [weak self] videos in
@@ -96,33 +96,33 @@ final class VideoRecordInteractor: PresentableInteractor<VideoRecordPresentable>
                 self.videosSubject.send(videos)
             })
             .store(in: &self.cancellables)
-
+        
         Task {
             let isAuthorized = await self.recordingManager.requestAuthorization(aspectRatio: .oneToOne)
             self.cameraAuthorizationSubject.send(isAuthorized)
         }
     }
-
+    
     private func saveVideo(url: URL) async {
         guard let thumbnail = url.generateThumbnail(),
               let thumbnailData = thumbnail.jpegData(compressionQuality: 0.8) else {
             print("썸네일 생성 실패")
             return
         }
-
+        
         do {
             let duration = try await url.videoDuration()
-
+            
             let draft = VideoDraft(
                 duration: duration,
                 thumbnail: thumbnailData
             )
-
+            
             let filePath = try self.videoDraftStorage?.saveVideoDraft(sourceURL: url, fileName: draft.fileBaseName)
             var drafts = try self.videoDraftStorage?.loadAll(type: VideoDraft.self) ?? []
             drafts.append(draft)
             try self.videoDraftStorage?.updateBackup(drafts)
-
+            
             self.videoSubject.send(drafts)
             print("저장 성공 \(filePath?.lastPathComponent ?? "")")
         } catch {
@@ -151,22 +151,22 @@ extension VideoRecordInteractor {
 extension VideoRecordInteractor {
     func didTapRecord() {
         let duration = TimeInterval(durationOptions[currentDurationIndex])
-
+        
         self.isRecordingSubject.send(true)
         self.recordDurationSubject.send(duration)
-
+        
         Task {
             do {
                 let url = try await self.recordingManager.recordVideo(duration: duration)
-
+                
                 self.isRecordingSubject.send(false)
                 self.recordDurationSubject.send(0)
-
+                
                 await self.saveVideo(url: url)
             } catch {
                 self.isRecordingSubject.send(false)
                 self.recordDurationSubject.send(0)
-
+                
                 if let cameraError = error as? CameraError {
                     switch cameraError {
                     case .cancelled:
@@ -180,24 +180,26 @@ extension VideoRecordInteractor {
             }
         }
     }
-
+    
     func didTapFlip() {
         self.recordingManager.switchCamera()
     }
-
+    
     func didTapRatio() {
         self.currentAspectRatio = self.currentAspectRatio.next()
         self.aspectRatioSubject.send(self.currentAspectRatio)
         self.ratioButtonTextSubject.send(self.currentAspectRatio.rawValue)
         self.recordingManager.updateAspectRatio(aspectRatio: self.currentAspectRatio)
     }
-
+    
     func didTapTimer() {
         self.currentDurationIndex = (self.currentDurationIndex + 1) % self.durationOptions.count
-        let selected = self.durationOptions[self.currentDurationIndex]
+        guard let selected = self.durationOptions[safe: self.currentDurationIndex] else {
+            return self.timerButtonTextSubject.send(0)
+        }
         self.timerButtonTextSubject.send(selected)
     }
-
+    
     func didTapAlbum() {
         // 앨범 화면 이동
     }
